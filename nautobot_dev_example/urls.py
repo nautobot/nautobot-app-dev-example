8dcf01b6e0cf4008861cf720414756dab0123a6d
Gary Snider <75227981+gsnider2195@users.noreply.github.com>
2024-10-01 09:26:43 -0700

    add redirectview for NautobotAppConfig.docs_view_name (#49)
    * add redirectview for NautobotAppConfig.docs_view_name
    
    * changelog
    
    * changelog

--- nautobot_dev_example/urls.py
+++ nautobot_dev_example/urls.py
@@ -1,5 +1,8 @@
 """Django urlpatterns declaration for nautobot_dev_example app."""
 
+from django.templatetags.static import static
+from django.urls import path
+from django.views.generic import RedirectView
 from nautobot.apps.urls import NautobotUIViewSetRouter
 
 from nautobot_dev_example import views
@@ -7,4 +10,8 @@ from nautobot_dev_example import views
 router = NautobotUIViewSetRouter()
 router.register("devexample", views.DevExampleUIViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("docs/", RedirectView.as_view(url=static("nautobot_dev_example/docs/index.html")), name="docs"),
+]
+
+urlpatterns += router.urls
