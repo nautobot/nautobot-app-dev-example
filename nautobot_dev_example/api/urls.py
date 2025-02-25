2adb567de2cd38db6f90ae7495758ee0b4a878a6
Stephen Kiely <stephenkiely87@gmail.com>
2025-02-25 15:18:36 -0600

    Apply code suggestions

--- nautobot_dev_example/api/urls.py
+++ nautobot_dev_example/api/urls.py
@@ -4,9 +4,9 @@ from nautobot.apps.api import OrderedDefaultRouter
 
 from nautobot_dev_example.api import views
 
-app_name = "nautobot_dev_example-api"
 router = OrderedDefaultRouter()
 # add the name of your api endpoint, usually hyphenated model name in plural, e.g. "my-model-classes"
 router.register("devexample", views.DevExampleViewSet)
 
+app_name = "nautobot_dev_example-api"
 urlpatterns = router.urls
