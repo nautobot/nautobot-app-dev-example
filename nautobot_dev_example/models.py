6b9421945b68e569b83edd692db0ea2e7e95da04
Stephen Kiely <stephenkiely87@gmail.com>
2025-04-24 10:40:28 -0500

    Update nautobot_dev_example/models.py
    Co-authored-by: Gary Snider <75227981+gsnider2195@users.noreply.github.com>

--- nautobot_dev_example/models.py
+++ nautobot_dev_example/models.py
@@ -10,7 +10,7 @@ from nautobot.apps.models import PrimaryModel, extras_features
 # If you want to choose a specific model to overload in your class declaration, please reference the following documentation:
 # how to chose a database model: https://docs.nautobot.com/projects/core/en/stable/plugins/development/#database-models
 # If you want to use the extras_features decorator please reference the following documentation
-# https://docs.nautobot.com/projects/core/en/latest/plugins/development/#using-the-extras_features-decorator-for-graphql
+# https://docs.nautobot.com/projects/core/en/stable/development/core/model-checklist/#extras-features
 @extras_features(
     "custom_links", "custom_validators", "export_templates", "graphql", "relationships", "graphql", "webhooks"
 )
