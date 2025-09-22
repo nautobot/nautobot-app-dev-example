a171d7be0baea270726846430a2987d9582fd935
Roberto Duarte <roberto.duarte@codilime.com>
2025-09-22 18:30:20 -0300

    version fix

--- tasks.py
+++ tasks.py
@@ -52,7 +52,7 @@ namespace = Collection("nautobot_dev_example")
 namespace.configure(
     {
         "nautobot_dev_example": {
-            "nautobot_ver": "2.4.2",
+            "nautobot_ver": "2.4.11",
             "project_name": "nautobot-dev-example",
             "python_ver": "3.11",
             "local": False,
